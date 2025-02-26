18dbb74eda60d81948b5c3191d50354dedbe0d5b
Ananya Srivastava <124085911+sitcode0904@users.noreply.github.com>
2025-02-26 11:17:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -166,11 +166,17 @@ def getprofile(id):
     profile = cursor.fetchone()
     conn.close()
     return profile
+    
+@app.route("/")
+def home():
+    return "Hello, Flask is running!"
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default Render port
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
