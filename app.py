8c00d718b66120508ee6c640d58d61b04b893eed
Ananya Srivastava <124085911+sitcode0904@users.noreply.github.com>
2025-02-26 12:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -170,8 +170,7 @@ def getprofile(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-
+    app.run(host="0.0.0.0", port=5000, debug=False)  # Set debug=False
 
 
 
